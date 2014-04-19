37e68ff7f2dcace78b40bf4a773a8e3d67df9753
Ross Dowthwaite <rossdowthwaite@gmail.com>
2014-04-19 13:22:24 +0100

    hvvhvjhjbhbdcdscsdhxcvcvxccvvfvfvfdv

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery-ui
 //= require twitter/bootstrap
 //= require best_in_place
 //= require_tree .
@@ -24,6 +24,7 @@
 
 $(document).ready(function() {
   alert('hello');
+  $("#students ,#prices ").nestedFields();
 });
 
 
